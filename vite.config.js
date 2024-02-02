b442cec5cbac6d88fed7e74cdd87a188bbf5dd2c
unknown <khayatiadel10@gmail.com>
2024-02-02 16:37:35 +0100

    add

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api/": "https://server-shop-9uv4.onrender.com",
-      "/uploads/": "https://server-shop-9uv4.onrender.com",
-    },
     // proxy: {
-    //   "/api/": "http://localhost:8000",
-    //   "/uploads/": "http://localhost:8000",
+    //   "/api/": "https://server-shop-9uv4.onrender.com",
+    //   "/uploads/": "https://server-shop-9uv4.onrender.com",
     // },
+    proxy: {
+      "/api/": "http://localhost:8000",
+      "/uploads/": "http://localhost:8000",
+    },
   },
 
 });
